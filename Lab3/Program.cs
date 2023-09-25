3cfb44a9f698cbd6aa5c94d7b7309265ee3a1240
Mikhail-Kolpakov <mihailkolp@gmail.com>
2023-09-25 22:15:45 +0300

    Lab. 3

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -2,21 +2,33 @@
 using System.Text;
 
 const int givenPerc = 75;
-int N = 9 * 5 + 50;
-int S = (int)Math.Round(N * ((double)givenPerc / 100));
+const int N = 9 * 5 + 50;
+int S = (int)(N * ((double)givenPerc / 100));
 Random rand = new Random();
-Dictionary<int, int> numbers = new Dictionary<int, int>();
+Dictionary<int, int> numbers = new Dictionary<int, int>(S);
 
 Console.OutputEncoding = Encoding.UTF8;
 
-for (int i = 0; i < S; i++)
-    numbers.Add(i, rand.Next(1, 1001)); //Генеруємо випадкові значення
-
-Console.WriteLine("Всі елементи після операції видалення парних чисел: ");
-for (int i = 0; i < numbers.Count; i++)
+//Додаємо значення до хеш-таблиці
+for (int i = 0; i < N; i++)
 {
-    if (numbers[i] % 2 == 0) //Якщо число парне
-        numbers.Remove(i);
-    else
-        Console.Write($"{numbers[i]} ");
+    int random_number;
+    do
+    {
+        random_number = rand.Next(-100, 101);
+    }
+    while (numbers.ContainsKey(random_number));
+    numbers.Add(random_number, random_number);
 }
+
+//Видаляємо парні числа з хеш-таблиці
+foreach (KeyValuePair<int, int> number in numbers)
+    if (number.Value % 2 == 0)
+        numbers.Remove(number.Key);
+
+//Виводимо кінцеві непарні значення
+Console.WriteLine("Кінцеві непарні значення з хеш-таблиці: ");
+foreach (KeyValuePair<int, int> number in numbers)
+    Console.Write($"{number.Value} ");
+
+Console.WriteLine();
